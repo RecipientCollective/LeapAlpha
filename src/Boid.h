55a20a8425c42ffb27704d14b890d28eaec297dd
tucano <davide.rambaldi@gmail.com>
2015-05-29 10:49:41 +0200

    Added missing header

---
+++ src/Boid.h
@@ -0,0 +1,35 @@
+#ifndef BOID_H
+#define BOID_H
+
+#include "ofMain.h"
+//#include "ofxVectorMath.h"
+
+class Boid {
+public:
+	Boid();
+	Boid(int x, int y);
+	
+	void update(vector<Boid> &boids);
+	void draw();
+	
+    void seek(ofVec2f target);
+    void avoid(ofVec2f target);
+    void arrive(ofVec2f target);
+	
+    void flock(vector<Boid> &boids);
+    bool isHit(int x,int y, int radius);
+    
+	ofVec2f steer(ofVec2f target, bool slowdown);
+	
+	ofVec2f separate(vector<Boid> &boids);
+	ofVec2f align(vector<Boid> &boids);
+	ofVec2f cohesion(vector<Boid> &boids);
+	
+	ofVec2f loc,vel,acc;
+    
+	float r;
+	float maxforce;
+	float maxspeed;
+};
+
+#endif
